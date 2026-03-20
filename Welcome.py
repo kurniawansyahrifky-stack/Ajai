512382ce95c21a7f22594d4ce1df9611a3f15739
kurniawansyahrifky-stack <kurniawansyahrifky@gmail.com>
2026-03-20 17:12:58 +0700

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -424,7 +424,7 @@ WELC_HELP_TXT = "Your group's welcome/goodbye messages can be personalised in mu
                 "Simply replace `group_id` with your group's id, which can be obtained via /id, and you're good to " \
                 "go. Note that group ids are usually preceded by a `-` sign; this is required, so please don't " \
                 "remove it.\n" \
-                "If you're feeling fun, you can even set images/gifs/videos/voice
+                "If you're feeling fun, you can even set images/gifs/videos/voice messages as the welcome message by replying to the desired media, and calling /setwelcome."
 
 messages as the welcome message by replying to the desired media, and calling /setwelcome.".format(dispatcher.bot.username)
 
